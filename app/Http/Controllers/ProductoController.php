3c0ff90438366f898cda7e5e1c63a3c514625b13
AlejandroJMP24 <alejandromachado85@gmail.com>
2023-11-02 17:58:07 -0300

    cambios el cargar y agregar productos

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Moneda;
 use Illuminate\Http\Request;
 use App\Models\Producto;
 use Illuminate\Support\Facades\Validator;
@@ -11,12 +12,46 @@ class ProductoController extends Controller
 {
     public function MostrarTodosLosProductos(Request $Request)
     {
-        return Producto::all();
+        $datoProducto = Producto::withTrashed()->get();
+        $infoProducto = [];
+        if ($datoProducto) {
+            foreach ($datoProducto as $dato) {
+                $infoProducto[] = $this->obtenerProducto($dato);
+            }
+        }
+        $moneda = $this->obtenerMonedas();
+        return [$infoProducto, $moneda];
     }
 
     public function MostrarUnproducto(Request $request, $idProducto)
     {
-        return Producto::findOrFail($idProducto);
+        $datoProducto = Producto::findOrFail($idProducto);
+        $infoProducto = [];
+        if ($datoProducto) {
+            $infoProducto[] = $this->obtenerProducto($datoProducto);
+        }
+        return [$infoProducto];
+    }
+
+    private function obtenerProducto($producto)
+    {
+        try {
+            $moneda = Moneda::withTrashed()->where('id', $producto['id_moneda'])->first();
+            return ([
+                'Id' => $producto['id'],
+                'Nombre' => $producto['nombre'],
+                'Stock' => $producto['stock'],
+                'Precio' => $producto['precio'],
+                'Moneda' => $moneda['moneda'],
+                'created_at' => $producto['created_at'],
+                'updated_at' => $producto['updated_at'],
+                'deleted_at' => $producto['deleted_at']
+            ]);
+        } catch (\Exception $e) {
+            $mensajeDeError = 'Error: ';
+            return $mensajeDeError;
+        }
+
     }
 
     public function ComprobarDatosProducto(Request $request)
@@ -35,18 +70,19 @@ class ProductoController extends Controller
                 'nombre' => 'required|regex:/^[\pL\s\-]+$/u|max:50|min:3',
                 'precio' => 'required|numeric|max:1000|min:1',
                 'stock' => 'required|numeric|max:1000|min:1',
-                'idMoneda' => 'required|numeric'
+                'idMoneda' => 'required|string'
             ],
         );
     }
 
-    public function IngresarUnProducto(Request $request)
+    public function IngresarUnProducto($request)
     {
+        $moneda=$this->devolverMoneda($request->post('idMoneda'));
         $Producto = new Producto;
         $Producto->nombre = $request->post("nombre");
         $Producto->precio = $request->post("precio");
         $Producto->stock = $request->post("stock");
-        $Producto->id_moneda = $request->post("idMoneda");
+        $Producto->id_moneda = $moneda;
 
         $Producto->save();
 
@@ -63,22 +99,31 @@ class ProductoController extends Controller
 
     public function Modificar(Request $request, $idProducto)
     {
-        $Producto = Producto::findOrFail($idProducto);
-        $Producto->nombre = $request->post("nombre");
-        $Producto->precio = $request->post("precio");
-        $Producto->stock = $request->post("stock");
-        $Producto->id_moneda = $request->post("idMoneda");
-
-        $Producto->save();
-
+        $moneda=$this->devolverMoneda($request->post('idMoneda'));
+        $Producto = Producto::where('id', $idProducto)->update([
+            'nombre' => $request->post('nombre'),
+            'precio' => $request->post('precio'),
+            'stock' => $request->post('stock'),
+            'id_moneda' => $moneda
+        ]);
         return $Producto;
     }
 
     public function restore($id)
     {
         Producto::withTrashed()->find($id)->restore();
-
         return back();
+    }
+
+    public function obtenerMonedas()
+    {
+        return Moneda::all(['moneda']);
+    }
+
+    public function devolverMoneda($moneda)
+    {
+        $idMoneda = Moneda::where('moneda', $moneda)->first();
+        return $idMoneda['id'];
     }
 
 }
