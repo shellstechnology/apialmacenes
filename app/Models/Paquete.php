6f0638d99ccc7163493a025fdda81f97180bf9ae
Matias Meneses <mat.meneses47@gmail.com>
2023-11-04 19:12:33 -0300

    todo finalizado para que actions funcione

--- app/Models/Paquete.php
+++ app/Models/Paquete.php
@@ -11,6 +11,7 @@ class Paquete extends Model
     use SoftDeletes;
     use HasFactory;
     protected $table = "paquetes";
+    protected $fillable = ['nombre','volumen_l','peso_kg','id_estado_p','id_caracteristica_paquete','id_lugar_entrega','nombre_destinatario','nombre_remitente'];
     public $timestamps = true;
     
 }
